9961b2cc17d6973411d99733801fc49c86cbb2ce
Oreoluwa Sokunbi <osokunbi@umich.edu>
2024-05-22 23:58:10 -0400

    thread-safety

---
+++ README.md
@@ -0,0 +1,7 @@
+### in-memory queue that lets you enqueue/dequeue items in constant time using an api endpoint
+
+this is a simple in memory queue that lets you enqueue/dequeue items in constant time using an api endpoint. the queue is implemented using a doubly linked list. the queue is thread safe and can be used by multiple threads concurrently.
+
+this is really just something i did out of boredom, though it could be useful in some cases. i'll probably use it in the future for something i guess.
+
+## peace :P
--- queue-backend/queue/queue.go
+++ queue-backend/queue/queue.go
@@ -1,104 +1,83 @@
 package queue
 
 import (
-  "fmt"
-  "errors"
+    "errors"
+    "sync"
 )
 
 type Node struct {
-  Value  interface{}
-  prev *Node
-  next *Node
+    Value interface{}
+    prev  *Node
+    next  *Node
 }
 
 type Queue struct {
-  Head *Node
-  Tail *Node
-  size   int
+    Head *Node
+    Tail *Node
+    size int
+    mu   sync.Mutex 
 }
 
 func NewQueue() *Queue {
-  return &Queue {
-    Head: nil,
-    Tail: nil,
-    size:   0,
-  }
-}
-func (l *Queue) Enqueue(data interface{} ){
-  newNode := &Node {
-    Value: data,
-  }
-
-  l.size++
-  if l.Head == nil {
-    l.Head = newNode
-    l.Tail = newNode
-    return
-  }
-  
-  l.Tail.next = newNode
-  newNode.prev = l.Tail
-  l.Tail = newNode
-  return
-}
-
-func (l *Queue) Pop() (interface{}, error){
-  if l.Head == nil {
-    return nil, errors.New("Tried to pop from an empty list!")
-  }
-
-  l.size--
-  value := l.Tail.Value
-  if l.Head == l.Tail {
-    l.Head = nil
-    l.Tail = nil
-    return value, nil
-  }
-  l.Tail = l.Tail.prev
-  l.Tail.next = nil
-  return value, nil
-}
-
-func (l *Queue) PushFront(data interface{}) {
-  l.size++
-  newNode := &Node{
-    Value: data,
-  }
-
-  if l.Head == nil {
-    l.Head = newNode
-    l.Tail = newNode
-  }
-
-  l.Head.prev = newNode
-  newNode.next = l.Head
-  l.Head = newNode
-}
-
-func (l *Queue) Dequeue() (interface{}, error){
-  if l.Head == nil {
-    return nil, errors.New("tried to pop from an empty list!")
-  }
-  l.size--
-
-  value := l.Head.Value
-  if l.Head == l.Tail {
-    l.Head = nil
-    l.Tail = nil
-    return value, nil
-  }
-
-  l.Head = l.Head.next
-  l.Head.prev = nil
-  return value, nil
-}
-
-func (l *Queue) Clear() {
-  for ; l.Head != nil; {
-    _, err := l.Dequeue()
-    if err != nil {
-      fmt.Println("Error occured while clearing list")
-      break
+    return &Queue{
+        Head: nil,
+        Tail: nil,
+        size: 0,
     }
-  }
+}
+
+func (q *Queue) Enqueue(data interface{}) {
+    q.mu.Lock()
+    defer q.mu.Unlock()
+
+    newNode := &Node{
+        Value: data,
+    }
+
+    q.size++
+    if q.Head == nil {
+        q.Head = newNode
+        q.Tail = newNode
+        return
+    }
+
+    q.Tail.next = newNode
+    newNode.prev = q.Tail
+    q.Tail = newNode
+}
+
+func (q *Queue) Dequeue() (interface{}, error) {
+    q.mu.Lock()
+    defer q.mu.Unlock()
+
+    if q.Head == nil {
+        return nil, errors.New("queue is empty")
+    }
+
+    q.size--
+
+    value := q.Head.Value
+    if q.Head == q.Tail {
+        q.Head = nil
+        q.Tail = nil
+        return value, nil
+    }
+
+    q.Head = q.Head.next
+    q.Head.prev = nil
+    return value, nil
+}
+
+func (q *Queue) Size() int {
+    q.mu.Lock()
+    defer q.mu.Unlock()
+    return q.size
+}
+
+func (q *Queue) Clear() {
+    q.mu.Lock()
+    defer q.mu.Unlock()
+    q.Head = nil
+    q.Tail = nil
+    q.size = 0
 }
